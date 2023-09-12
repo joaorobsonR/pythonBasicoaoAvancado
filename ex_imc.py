c68a8265e8ba9145df551aad2ceb1b9cd1ebbf8f
joaorobsonR <jrobsonramalho@gmail.com>
2023-09-11 22:03:33 -0300

    adaptando exercicio

--- ex_imc.py
+++ ex_imc.py
@@ -1,7 +1,22 @@
-nome = input(str('Insira seu nome: '))
-peso = 80
-altura = 1.74
-imc = peso / (altura * altura)
+nome = input('Insira seu nome: ')
+peso = input('peso: ')
+altura = input('altura: ')
 
-print('{}, o calculo do seu IMC é {}'.format(nome, imc))
-print(f'{nome} seu IMC é {imc:.2f}')
+peso_num = float(peso)
+altura_num = float(altura)
+
+imc = peso_num / (altura_num**2)
+
+if imc >= 27 and imc < 40:
+    print('{}, o calculo do seu IMC é {} indica sobre peso'.format(nome, imc))
+
+elif imc < 27 and imc > 15:
+    print('{}, o calculo do seu IMC é {} indica ideal'.format(nome, imc))
+
+elif imc < 15:
+    print('{}, o calculo do seu IMC é {} indica desnutrição'.format(nome, imc))
+
+else:
+    print('{}, o calculo do seu IMC é {} indica obsidade morbidada'.format(nome, imc))
+
+print('fim do laço')
